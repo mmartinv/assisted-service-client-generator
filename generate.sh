69ecf2ef8d8f16b0170803c304be5172d3d8dd88
Miguel Martín <mmartinv@redhat.com>
2021-05-07 11:49:09 +0200

    Update messages

--- generate.sh
+++ generate.sh
@@ -15,7 +15,7 @@ ASSISTED_SERVICE_DIR="assisted-service-client-${CLIENT}-${ASSISTED_SERVICE_VERSI
 PACKAGE_VERSION=${ASSISTED_SERVICE_VERSION}
 [ "${PACKAGE_VERSION}" != "master" ] || PACKAGE_VERSION="0.0.0"
 
-echo "Generating the Assisted Service API Python client"
+echo "Generating the '${CLIENT}' client"
 OPENAPI_DOWNLOAD_URL=$(curl -s -H "Content-Type: application/json" -X "POST" -d "{
   \"${SPEC_FIELD}\": \"https://raw.githubusercontent.com/openshift/assisted-service/${ASSISTED_SERVICE_VERSION}/swagger.yaml\",
   \"options\": {
@@ -26,13 +26,11 @@ OPENAPI_DOWNLOAD_URL=$(curl -s -H "Content-Type: application/json" -X "POST" -d 
   }
 }" "${OPENAPI_GENERATOR_URL}/api/gen/clients/${CLIENT}" | jq -r .link)
 
-echo "Downloading the Assisted Service API Python client"
+echo "Downloading the '${CLIENT}' client"
 curl -s -o "${CLIENT_FILENAME}" "${OPENAPI_DOWNLOAD_URL}"
 
-echo "Unpacking the Assisted Service API Python client"
+echo "Unpacking the and updating the '${CLIENT}' client"
 unzip -qq "${CLIENT_FILENAME}"
-
-echo "Cleaning"
 rm -rf "${ASSISTED_SERVICE_DIR}"
 mv "${CLIENT_DIR}" "${ASSISTED_SERVICE_DIR}"
 rm -f "${CLIENT_FILENAME}"
